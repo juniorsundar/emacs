853fb0405243ba780a6012663b48047c154aeaa2
Junior Sundar <juniorsundar@gmail.com>
2024-12-09 22:35:43 +0400

    Removed config.org

--- init.el
+++ init.el
@@ -742,7 +742,7 @@
   ;; (vertico-scroll-margin 0) ;; Different scroll margin
   ;; (vertico-count 20) ;; Show more candidates
   (vertico-resize t) ;; Grow and shrink the Vertico minibuffer
-  ;; (vertico-cycle t) ;; Enable cycling for `vertico-next/previous'
+  (vertico-cycle t) ;; Enable cycling for `vertico-next/previous'
   :init
   (vertico-mode))
 
