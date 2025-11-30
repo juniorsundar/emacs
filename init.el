1f63ae6c883a862539a784f31aab12bf0c32f553
Junior Sundar <juniorsundar@gmail.com>
2025-11-30 11:59:20 -0800

    Revert to meow

--- init.el
+++ init.el
@@ -849,60 +849,103 @@
 ;;-----------------------------------------------------------------------------
 ;; Meow
 ;;-----------------------------------------------------------------------------
-(use-package evil
-  :init
-  (evil-mode)
+(use-package meow
+  :ensure t
   :config
-  (evil-set-initial-state 'eat-mode 'insert)
-  :custom
-  (evil-want-keybinding nil)    ;; Disable evil bindings in other modes (It's not consistent and not good)
-  (evil-want-C-u-scroll t)      ;; Set C-u to scroll up
-  (evil-want-C-i-jump nil)      ;; Disables C-i jump
-  (evil-undo-system 'undo-redo) ;; C-r to redo
-  ;; Unmap keys in 'evil-maps. If not done, org-return-follows-link will not work
-  :bind (:map evil-motion-state-map
-			  ("SPC" . nil)
-			  ("RET" . nil)
-			  ("TAB" . nil)
-              ;; Window Management
-              ;; ("C-M-<up>" . evil-window-increase-height)
-              ;; ("C-M-k" . evil-window-increase-height)
-              ;; ("C-M-<down>" . evil-window-decrease-height)
-              ;; ("C-M-j" . evil-window-decrease-height)
-              ;; ("C-M-<right>" . evil-window-increase-width)
-              ;; ("C-M-l" . evil-window-increase-width)
-              ;; ("C-M-<left>" . evil-window-decrease-width)
-              ;; ("C-M-h" . evil-window-decrease-width)
-              )
-  ;; (:map evil-visual-state-map
-  ;; 		("M-h"      . evil-shift-left)
-  ;; 		("M-l"      . evil-shift-right)
-  ;; 		("M-<left>" . evil-shift-left)
-  ;; 	("M-<right>". evil-shift-right)
-  ;; 		)
+  (defun meow-setup ()
+    (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
+    (meow-motion-define-key
+     '("j" . meow-next)
+     '("k" . meow-prev)
+     '("<escape>" . ignore))
+    (meow-leader-define-key
+     ;; Use SPC (0-9) for digit arguments.
+     '("1" . meow-digit-argument)
+     '("2" . meow-digit-argument)
+     '("3" . meow-digit-argument)
+     '("4" . meow-digit-argument)
+     '("5" . meow-digit-argument)
+     '("6" . meow-digit-argument)
+     '("7" . meow-digit-argument)
+     '("8" . meow-digit-argument)
+     '("9" . meow-digit-argument)
+     '("0" . meow-digit-argument)
+     '("/" . meow-keypad-describe-key)
+     '("?" . meow-cheatsheet))
+    (meow-normal-define-key
+     '("0" . meow-expand-0)
+     '("9" . meow-expand-9)
+     '("8" . meow-expand-8)
+     '("7" . meow-expand-7)
+     '("6" . meow-expand-6)
+     '("5" . meow-expand-5)
+     '("4" . meow-expand-4)
+     '("3" . meow-expand-3)
+     '("2" . meow-expand-2)
+     '("1" . meow-expand-1)
+     '("-" . negative-argument)
+     '(";" . meow-reverse)
+     '("," . meow-inner-of-thing)
+     '("." . meow-bounds-of-thing)
+     '("[" . meow-beginning-of-thing)
+     '("]" . meow-end-of-thing)
+     '("a" . meow-append)
+     '("A" . meow-open-below)
+     '("b" . meow-back-word)
+     '("B" . meow-back-symbol)
+     '("c" . meow-change)
+     '("d" . meow-delete)
+     '("D" . meow-backward-delete)
+     '("e" . meow-next-word)
+     '("E" . meow-next-symbol)
+     '("f" . meow-find)
+     '("g" . meow-cancel-selection)
+     '("G" . meow-grab)
+     '("h" . meow-left)
+     '("H" . meow-left-expand)
+     '("i" . meow-insert)
+     '("I" . meow-open-above)
+     '("j" . meow-next)
+     '("J" . meow-next-expand)
+     '("k" . meow-prev)
+     '("K" . meow-prev-expand)
+     '("l" . meow-right)
+     '("L" . meow-right-expand)
+     '("m" . meow-join)
+     '("n" . meow-search)
+     '("o" . meow-block)
+     '("O" . meow-to-block)
+     '("p" . meow-yank)
+     '("q" . meow-quit)
+     '("Q" . meow-goto-line)
+     '("r" . meow-replace)
+     '("R" . meow-swap-grab)
+     '("s" . meow-kill)
+     '("t" . meow-till)
+     '("u" . meow-undo)
+     '("U" . meow-undo-in-selection)
+     '("v" . meow-visit)
+     '("w" . meow-mark-word)
+     '("W" . meow-mark-symbol)
+     '("x" . meow-line)
+     '("X" . meow-goto-line)
+     '("y" . meow-save)
+     '("Y" . meow-sync-grab)
+     '("z" . meow-pop-selection)
+     '("'" . repeat)
+     '("<left>"  . meow-left)
+     '("<right>" . meow-right)
+     '("<up>"    . meow-prev)
+     '("<down>"  . meow-next)
+     '("<S-left>"  . meow-left-expand)
+     '("<S-right>" . meow-right-expand)
+     '("<S-up>"    . meow-prev-expand)
+     '("<S-down>"  . meow-next-expand)
+     '("<escape>" . ignore)))
+  (require 'meow)
+  (meow-setup)
+  (meow-global-mode 1)
   )
-
-(use-package evil-surround
-  :ensure t
-  :after evil
-  :config
-  (global-evil-surround-mode 1))
-
-(use-package evil-collection
-  :ensure t
-  :after evil
-  :config
-  (evil-collection-init))
-
-(use-package evil-commentary
-  :after evil
-  :config (evil-commentary-mode))
-
-(use-package evil-avy
-  :after (evil avy)
-  :config
-  (evil-define-key '(normal visual) 'global "s" #'avy-goto-word-1))
-
 
 (use-package multiple-cursors
   :ensure t
