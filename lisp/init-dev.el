9e7e123127b9dd25adf11261a0ce49e0bcf04765
Junior Sundar <juniorsundar@gmail.com>
2026-01-27 19:59:51 +0400

    Init-dev updated with lsp ui keymaps

--- lisp/init-dev.el
+++ lisp/init-dev.el
@@ -96,7 +96,19 @@
   :ensure t
   :bind (:map lsp-ui-mode-map
               ([remap xref-find-definitions] . #'lsp-ui-peek-find-definitions)
-              ([remap xref-find-references] . #'lsp-ui-peek-find-references)))
+              ([remap xref-find-references] . #'lsp-ui-peek-find-references)
+              ("M-i" . #'lsp-ui-doc-focus-frame))
+  :config
+  (setq lsp-ui-doc-position 'at-point)
+
+  (defun my/lsp-ui-doc-quit ()
+    (interactive)
+    (lsp-ui-doc-unfocus-frame)
+    (lsp-ui-doc-hide))
+
+  (with-eval-after-load 'lsp-ui-doc
+    (define-key lsp-ui-doc-frame-mode-map "q" #'my/lsp-ui-doc-quit)
+    (define-key lsp-ui-doc-frame-mode-map (kbd "C-g") #'my/lsp-ui-doc-quit)))
 
 (defun my/lsp-flymake-only ()
   "Ensure only LSP diagnostics are used in Flymake."
