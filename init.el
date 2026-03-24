35d5a12b343465981683867510d4c62610652e0e
Junior Sundar <junior.sundar@tii.ae>
2026-03-24 14:10:05 +0400

    Remove AI Garbage

--- init.el
+++ init.el
@@ -1197,16 +1197,4 @@ This is a non-interactive helper function."
 (provide 'init-keys)
 ;;; init-keys.el ends here
 
-;;; init-ai.el --- AI Garbage goes in here -*- lexical-binding: t -*-
-
-;;; Code:
-
-(use-package agent-shell
-    :ensure t
-    ;; Add agent installation configs here
-    )
-
-(provide 'init-ai)
-;;; init-ai.el ends here
-
 ;;; init.el ends here
