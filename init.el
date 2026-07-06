dd7a020f94731b921f46e423643298eccf301cac
Junior Sundar <juniorsundar@gmail.com>
2026-07-06 21:35:34 +0400

    Some petty cleanup

--- init.el
+++ init.el
@@ -99,12 +99,9 @@
         ibuffer-filter-group-name-face '(:inherit (success bold))
         ibuffer-formats
         `((mark modified read-only locked
-                " " (all-the-icons 4 4 :left)
-                (name 18 18 :left :elide)
+                " " (name 18 18 :left :elide)
                 " " (size 9 -1 :right)
                 " " (mode 16 16 :left :elide)
-                ,@(when (require 'ibuffer-vc nil t)
-                    '(" " (vc-status 12 :left)))
                 " " filename-and-process)
           (mark " " (name 16 -1) " " filename)))
 
@@ -114,8 +111,6 @@
   ;; (evil-define-key 'normal ibuffer-mode-map "q" #'kill-current-buffer)
   )
 
-(use-package ibuffer-vc
-  :ensure t)
 
 (use-package dired
   :ensure nil                                                ;; This is built-in, no need to fetch it.
@@ -313,18 +308,19 @@
 
 (add-hook 'ibuffer-mode-hook (lambda () (display-line-numbers-mode -1)))
 
-(use-package all-the-icons
+(use-package nerd-icons
   :ensure t
   :if (display-graphic-p))
 
-(use-package all-the-icons-dired
+(use-package nerd-icons-ibuffer
   :ensure t
-  :after all-the-icons
-  :hook (dired-mode . all-the-icons-dired-mode))
+  :after nerd-icons
+  :hook (ibuffer-mode . nerd-icons-ibuffer-mode))
 
-(use-package all-the-icons-ibuffer
+(use-package nerd-icons-dired
   :ensure t
-  :hook (ibuffer-mode . all-the-icons-ibuffer-mode))
+  :after nerd-icons
+  :hook (dired-mode . nerd-icons-dired-mode))
 
 (use-package nerd-icons-corfu
   :ensure t
@@ -413,7 +409,7 @@
   :config
   (defun meow-setup ()
     (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
-    (meow-motion-define-key
+    (meow-motion-overwrite-define-key
      '("j" . meow-next)
      '("k" . meow-prev)
      '("<escape>" . ignore))
@@ -653,10 +649,6 @@ This is a non-interactive helper function."
   :config
   (add-hook 'prog-mode-hook (lambda () (flymake-mode -1))))
 
-(use-package flymake
-  :ensure nil
-  :custom
-  (flymake-fringe-indicator-position 'right-fringe))
 ;;-----------------------------------------------------------------------------
 ;; LSP and Language Modes
 ;;-----------------------------------------------------------------------------
@@ -808,174 +800,6 @@ This is a non-interactive helper function."
   :after (consult projectile))
 
 ;;-----------------------------------------------------------------------------
-;; Org-Mode
-;;-----------------------------------------------------------------------------
-(use-package org
-  :ensure t
-  :hook
-  (org-mode . org-indent-mode)
-  (org-mode . visual-line-mode)
-  :custom
-  (org-return-follows-link t)
-  :config
-  (setq org-startup-with-inline-images t)
-  (setq org-image-actual-width 500)
-  (setq org-directory "~/Dropbox/org/")
-  (setq org-todo-keywords
-	'((sequence "TODO(t)" "DOING(d!)" "HOLD(h)" "|" "DONE(D)" "CANCELLED(c)" "MAYBE(m)")))
-  (setq org-todo-keyword-faces
-	'(("DOING" . (:background "orange" :foreground "black"))
-	  ("DONE" . (:background "green" :foreground "black"))
-	  ("HOLD" . (:background "turquoise" :foreground "black"))
-	  ("TODO" . (:background "red" :foreground "white"))
-	  ("CANCELLED" . (:background "gray" :foreground "black"))
-	  ("MAYBE" . (:background "yellow" :foreground "black"))))
-  (setq org-agenda-prefix-format
-	'((agenda . " %i %?-12t% s")
-	  (todo . " %i ")
-	  (tags . " %i ")
-	  (search . " %i ")))
-  (setq org-log-done 'time)
-  (setq org-default-notes-file (concat org-directory "/inbox.org"))
-  (setq org-attach-id-dir "~/Dropbox/org/assets/")
-  (setq org-attach-use-inheritance t)
-  (setq org-capture-templates
-	'(("t" "Blank Todo [inbox]" entry
-	   (file+headline "~/Dropbox/org/inbox.org" "Tasks")
-	   "* TODO %i%?")
-	  ("w" "Work Todo [work]" entry
-	   (file+headline "~/Dropbox/org/work.org" "Work")
-	   "* TODO %i%?")
-	  ("p" "Personal Todo [personal]" entry
-	   (file+headline "~/Dropbox/org/personal.org" "Personal")
-	   "* TODO %i%?")))
-  (setq org-hide-emphasis-markers t)
-  )
-
-(defun my-org-agenda-files-recursive (directory)
-  "Recursively find all .org files in DIRECTORY."
-  (directory-files-recursively directory "\\.org$"))
-(defun my-update-org-agenda-files (&rest _)
-  "Update `org-agenda-files` to include all .org files in the directory."
-  (setq org-agenda-files (my-org-agenda-files-recursive "~/Dropbox/org/")))
-
-(advice-add 'org-agenda :before #'my-update-org-agenda-files)
-
-(defun my-kill-agenda-file-buffers ()
-  "Kill unmodified buffers visiting files listed in `org-agenda-files`."
-  (interactive)
-  (let ((killed-count 0)
-        (agenda-files (org-agenda-files t))
-        (agenda-files-set (make-hash-table :test 'equal)))
-    (dolist (file agenda-files)
-      (puthash (expand-file-name file) t agenda-files-set))
-
-    (dolist (buffer (buffer-list))
-      (let ((filename (buffer-file-name buffer)))
-        (when (and filename
-                   (gethash (expand-file-name filename) agenda-files-set)
-                   (not (buffer-modified-p buffer))
-                   (not (eq buffer (current-buffer)))
-                   (not (string-prefix-p "*" (buffer-name buffer))))
-          (kill-buffer buffer)
-          (setq killed-count (1+ killed-count)))))
-    (message "Killed %d agenda file buffers." killed-count)))
-
-(defface my-note-face
-  '((t :background "#a6d189" :foreground "#1e1e2e" :extend t))
-  "Face for NOTE blocks with full width background.")
-(defface my-important-face
-  '((t :background "#ea6962" :foreground "#1e1e2e" :extend t))
-  "Face for IMPORTANT blocks with full width background.")
-(defface my-warning-face
-  '((t :background "#e5c890" :foreground "#1e1e2e" :extend t))
-  "Face for WARNING blocks with full width background.")
-(defun apply-custom-org-block-face (start end face)
-  "Apply FACE to the entire visual line from START to END."
-  (let ((overlay (make-overlay start end)))
-    (overlay-put overlay 'face face)
-    (overlay-put overlay 'line-prefix
-                 nil)
-    (overlay-put overlay 'after-string
-                 (propertize " " 'face face 'display '(space :align-to right-margin)))))
-(defun highlight-org-block-region ()
-  "Highlight org blocks with full window width background."
-  (remove-overlays (point-min) (point-max))
-  (save-excursion
-    (goto-char (point-min))
-    (while (re-search-forward "^[ \t]*#\\+BEGIN_\\(NOTE\\|IMPORTANT\\|WARN\\)" nil t)
-      (let* ((block-type (match-string 1))
-             (block-start (line-beginning-position))
-             (face (pcase block-type
-                     ("NOTE" 'my-note-face)
-                     ("IMPORTANT" 'my-important-face)
-                     ("WARN" 'my-warning-face))))
-        (when (re-search-forward (format "^[ \t]*#\\+END_%s" block-type) nil t)
-          (apply-custom-org-block-face block-start (line-end-position) face))))))
-(define-minor-mode org-block-highlight-mode
-  "Minor mode for highlighting org blocks with full window width background."
-  :lighter " OrgBlockHL"
-  (if org-block-highlight-mode
-      (progn
-        (highlight-org-block-region)
-        (add-hook 'after-change-functions
-                  (lambda (&rest _) (highlight-org-block-region))
-                  nil t))
-    (remove-overlays (point-min) (point-max))))
-(add-hook 'org-mode-hook 'org-block-highlight-mode)
-
-(font-lock-mode 1)
-
-(defun my-org-faces ()
-  (set-face-attribute 'org-document-title nil :height 2.0 :family my/font-variable-family)
-  ;; (set-face-attribute 'org-level-1 nil :height 1.5 :family my/font-variable-family)
-  ;; (set-face-attribute 'org-level-2 nil :height 1.4 :family my/font-variable-family)
-  ;; (set-face-attribute 'org-level-3 nil :height 1.3 :family my/font-variable-family)
-  ;; (set-face-attribute 'org-level-4 nil :height 1.2 :family my/font-variable-family)
-  ;; (set-face-attribute 'org-level-5 nil :height 1.1 :family my/font-variable-family)
-  (set-face-attribute 'org-block nil :height 1.0 :family my/font-fixed-family)
-  (set-face-attribute 'org-code nil :height 1.0 :family my/font-fixed-family)
-  (set-face-attribute 'org-table nil :height 1.0 :family my/font-fixed-family)
-  )
-(add-hook 'org-mode-hook #'my-org-faces)
-;; (add-hook 'org-mode-hook #'variable-pitch-mode)
-
-(use-package toc-org
-  :commands toc-org-enable
-  :hook (org-mode . toc-org-mode))
-
-(use-package org-tempo
-  :ensure nil
-  :after org)
-
-(use-package org-modern
-  :after org
-  :config
-  (setq org-modern-star nil)
-
-  (setq org-agenda-tags-column 0)
-  (setq org-agenda-block-separator ?─)
-  (setq org-agenda-time-grid
-        '((daily today require-timed)
-          (800 1000 1200 1400 1600 1800 2000)
-          " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄"))
-  (setq org-agenda-current-time-string
-        "<-- now ───────")
-  (setq org-modern-priority nil)
-
-  (setq org-modern-todo-faces
-        (quote (("TODO" :background "red" :foreground "white")
-                ("DOING" :background "orange" :foreground "black")
-                ("HOLD" :background "turquoise" :foreground "black")
-                ("CANCELLED" . (:background "gray" :foreground "black"))
-                ("MAYBE" . (:background "yellow" :foreground "black"))
-                ("DONE" . (:background "green" :foreground "black")))))
-  (global-org-modern-mode)
-  )
-
-(add-hook 'org-mode-hook (lambda () (display-line-numbers-mode -1)))
-
-;;-----------------------------------------------------------------------------
 ;; General Keybindings
 ;;-----------------------------------------------------------------------------
 (use-package general
@@ -1055,27 +879,6 @@ This is a non-interactive helper function."
     "t" '(visual-line-mode :which-key "Visual Line")
     "l" '(display-line-numbers-mode :which-key "Line Numbers"))
 
-  ;; Define the "O" (Org) submap under the C-c leader
-  (general-def
-    :prefix "C-c O" ; Prefix for Org commands
-    "i" '(org-toggle-inline-images :which-key "Toggle Inline Images"))
-
-  ;; Define the "O t" (TODO States) sub-submap under the C-c O prefix
-  (general-def
-    :prefix "C-c O t" ; Prefix for Org TODO commands
-    "t" 'org-todo
-    "d" '((lambda () (interactive) (org-todo "DOING")) :which-key "DOING")
-    "h" '((lambda () (interactive) (org-todo "HOLD")) :which-key "HOLD")
-    "D" '((lambda () (interactive) (org-todo "DONE")) :which-key "DONE")
-    "c" '((lambda () (interactive) (org-todo "CANCELLED")) :which-key "CANCELLED")
-    "m" '((lambda () (interactive) (org-todo "MAYBE")) :which-key "MAYBE")
-    )
-
-  ;; Define the "O a" (Org Agenda) sub-submap under the C-c O prefix
-  (general-def
-    :prefix "C-c O a" ; Prefix for Org Agenda commands
-    "c" '(org-capture :which-key "Capture")
-    "a" '(org-agenda :which-key "Agenda"))
 
   ;; Define the "l" (LSP) submap under the C-c leader
   (general-def
@@ -1092,7 +895,7 @@ This is a non-interactive helper function."
   (general-def
     :prefix "C-c L D" ; Prefix for LSP Document commands
     "s" '(consult-imenu :which-key "Document Symbols")
-    "d" '(consult-flymake :which-key "Document Diagnostics"))
+    "d" '(consult-flycheck :which-key "Document Diagnostics"))
 
   ;; Define the "l W" (Workspace) sub-submap under the C-c l prefix
   (general-def
@@ -1100,7 +903,7 @@ This is a non-interactive helper function."
     ;; "a" 'projectile-add-known-project
     ;; "r" 'projectile-remove-known-project
     "s" '(consult-lsp-file-symbols :which-key "Workspace Symbols")
-    "d" '((lambda () (interactive) (consult-flymake t)) :which-key "Workspace Diagnostics"))
+    "d" '((lambda () (interactive) (consult-flycheck)) :which-key "Workspace Diagnostics"))
 
 
   (general-def 
